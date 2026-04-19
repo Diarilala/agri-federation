c2ac4d8e849717a81af6946b6bb0bfde3ca515d7
Diarilala <hei.diarilala@gmail.com>
2026-04-19 17:44:33 +0300

    feat: add spring-boot initialization

---
+++ src/main/java/com/agrifederation/AgriFederationApplication.java
@@ -0,0 +1,13 @@
+package com.agrifederation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriFederationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgriFederationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/agrifederation/AgriFederationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrifederation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriFederationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

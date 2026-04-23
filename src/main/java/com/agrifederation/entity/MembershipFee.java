294338e42340d52248b2009a93eb5162b03a69f7
Lavorary <hei.lavorary@gmail.com>
2026-04-23 10:10:43 +0300

    feat : implementing MembershipFeeRepository

--- src/main/java/com/agrifederation/entity/MembershipFee.java
+++ src/main/java/com/agrifederation/entity/MembershipFee.java
@@ -2,10 +2,12 @@ package com.agrifederation.entity;
 
 import com.agrifederation.enums.ActivityStatus;
 import com.agrifederation.enums.Frequency;
+import lombok.Data;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Data
 public class MembershipFee {
     private String id;
     private ActivityStatus status;
@@ -13,5 +15,6 @@ public class MembershipFee {
     private Frequency frequency;
     private float amount;
     private String label;
+    private
     private List<MemberPayment> memberPayments;
 }

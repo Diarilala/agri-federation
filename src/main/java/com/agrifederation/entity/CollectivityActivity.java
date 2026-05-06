92ddd5ae859f08ca03c004d02fa17ec955e07f6a
Lavorary <hei.lavorary@gmail.com>
2026-05-06 22:34:35 +0300

    feat : add missing attribute in CollectivityActivity

--- src/main/java/com/agrifederation/entity/CollectivityActivity.java
+++ src/main/java/com/agrifederation/entity/CollectivityActivity.java
@@ -5,15 +5,19 @@ import com.agrifederation.enums.Type;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 public class CollectivityActivity {
+    private String id;
     private String label;
     private Type activityType;
     private List<Occupation> memberOccupationConcerned;
+    private Integer weekOrdinal;
+    private DayOfWeek dayOfWeek;
     private MonthlyRecurrenceRule monthlyRecurrenceRule;
     private LocalDate executiveDate;
 }

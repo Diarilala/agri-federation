903ba6554c2d2825c894639a3b77d5391b8034e9
Lavorary <hei.lavorary@gmail.com>
2026-05-07 02:01:54 +0300

    feat : create controller for get attendance by activity id

--- src/main/java/com/agrifederation/controller/CollectivityActivityController.java
+++ src/main/java/com/agrifederation/controller/CollectivityActivityController.java
@@ -1,5 +1,6 @@
 package com.agrifederation.controller;
 
+import com.agrifederation.dto.ActivityMemberAttendanceDTO;
 import com.agrifederation.entity.CollectivityActivity;
 import com.agrifederation.exception.BadRequestException;
 import com.agrifederation.exception.NotFoundException;
@@ -37,6 +38,21 @@ public class CollectivityActivityController {
         List<CollectivityActivity> collectivityActivities;
         try{
             collectivityActivities = collectivityActivityService.getActivitiesByCollectivityId(id);
+            return ResponseEntity.status(HttpStatus.OK).body(collectivityActivities);
+    } catch (BadRequestException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}/activities/{activityId}/attendance")
+    public ResponseEntity<?> getAttendance(@PathVariable String id, @PathVariable String activityId) {
+        List<ActivityMemberAttendanceDTO> collectivityActivities;
+        try {
+            collectivityActivities = collectivityActivityService.getAttendanceByActivityId(id, activityId);
             return ResponseEntity.status(HttpStatus.OK).body(collectivityActivities);
     } catch (BadRequestException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());

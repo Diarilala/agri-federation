7d88e8aabcb287d02332552dfda14be3c7626a20
Lavorary <hei.lavorary@gmail.com>
2026-05-06 23:36:28 +0300

    feat : add missing attribute (idCollectivity and idMonthlyRecurrence) in CollectivityActivity

--- src/main/java/com/agrifederation/entity/CollectivityActivity.java
+++ src/main/java/com/agrifederation/entity/CollectivityActivity.java
@@ -5,7 +5,6 @@ import com.agrifederation.enums.Type;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -15,9 +14,9 @@ public class CollectivityActivity {
     private String id;
     private String label;
     private Type activityType;
+    private String idCollectivity;
+    private String idMonthlyRecurrence;
     private List<Occupation> memberOccupationConcerned;
-    private Integer weekOrdinal;
-    private DayOfWeek dayOfWeek;
     private MonthlyRecurrenceRule monthlyRecurrenceRule;
     private LocalDate executiveDate;
 }

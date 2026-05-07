7677f6fc67355923ce553e6cec499cd4f73282e0
Lavorary <hei.lavorary@gmail.com>
2026-05-07 12:32:40 +0300

    feat : correct wrong syntax for RequestMapping statistics

--- src/main/java/com/agrifederation/controller/ControllerStatisticsController.java
+++ src/main/java/com/agrifederation/controller/ControllerStatisticsController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/collectivites")
+@RequestMapping("/collectivities")
 @Data
 @AllArgsConstructor
 public class ControllerStatisticsController {
